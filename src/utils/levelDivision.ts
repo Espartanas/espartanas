131d7e684216bce3c9f82b0ad7acc33ef9497f27
Paulo <pvmg1@hotmail.com>
2024-06-05 13:42:03 -0300

    atualização dos textos do nivelamento

--- src/utils/levelDivision.ts
+++ src/utils/levelDivision.ts
@@ -2,12 +2,11 @@ export const levelDivision = [
   {
     level: 'iniciante',
     title: 'Bem-vindo ao seu ponto de partida!',
-    description: 'Nosso programa de treino para iniciantes é ideal para quem está começando a sua jornada no mundo do fitness ou retornando após um longo período de inatividade. Este plano foca em construir uma base sólida, com exercícios simples e eficazes que vão ajudar você a desenvolver força, resistência e flexibilidade.',
+    description: 'Nosso programa de treino iniciante é ideal para quem está começando a sua jornada no mundo do fitness ou retornando após um longo período de inatividade. Focamos em construir uma base sólida, com exercícios simples e eficazes que vão ajudar você a desenvolver força e resistência.',
     strengths: [
-      'Exercícios básicos: Movimentos fundamentais que preparam seu corpo para níveis mais altos de treino.',
+      'Exercícios básicos: Movimentos fundamentais que preparam seu corpo para níveis mais altos de treino ou para aprender do zero.',
       'Duração moderada: Sessões mais curtas para evitar sobrecarga e permitir uma adaptação gradual.',
       'Instruções detalhadas: Orientações passo a passo para garantir a execução correta e segura dos exercícios.',
-      'Acessível a todos: Treinos que podem ser realizados com pouco ou nenhum equipamento.',
     ],
     conclusion: 'Perfeito para quem quer começar com o pé direito e construir uma base sólida para progredir com segurança e confiança.'
   },
@@ -17,22 +16,21 @@ export const levelDivision = [
     description: 'Se você já possui uma base de condicionamento físico e está pronto para novos desafios, nosso programa intermediário é o ideal. Aqui, você encontrará treinos mais dinâmicos que aumentarão sua resistência, força e agilidade.',
     strengths: [
       'Variedade de exercícios: Movimentos que combinam diferentes técnicas para manter seu treino interessante e eficaz.',
-      'Intensidade moderada: Treinos mais longos e intensos que exigem mais esforço e dedicação.',
+      'Intensidade moderada: Treinos mais intensos que exigem mais esforço e dedicação.',
       'Técnicas avançadas: Introdução a exercícios mais complexos e desafiadores, com orientações para evitar lesões.',
-      'Uso de equipamentos: Inclusão de pesos livres, bandas de resistência e outros acessórios para potencializar os resultados.'
+      'Uso de equipamentos: Uso de equipamentos para trabalhar todos os grupos musculares com eficiência.'
     ],
     conclusion: 'Ideal para quem deseja sair da zona de conforto e ver melhorias significativas no seu condicionamento físico.'
   },
   {
     level: 'avancado',
     title: 'Desafie seus limites e conquiste novos objetivos!',
-    description: 'Nosso programa avançado é projetado para aqueles que já possuem um alto nível de condicionamento físico e buscam maximizar seu desempenho. Esses treinos intensos e variados são perfeitos para atletas experientes que querem levar suas habilidades ao extremo.',
+    description: 'Nosso programa avançado é projetado para aqueles que já se adaptaram o nível intermediário mas buscam se desafiar. Esse nível é perfeito para praticantes experientes que querem elevar suas habilidades e melhorar seu desempenho.',
     strengths: [
-      'Treinos intensivos: Sessões de alta intensidade com movimentos complexos que desafiam seu corpo e mente.',
       'Séries avançadas: Exercícios que exigem força, agilidade, resistência e coordenação máxima.',
       'Planejamento estratégico: Rotinas planejadas para otimizar o desempenho e evitar platôs.',
-      'Equipamentos variados: Uso avançado de equipamentos para trabalhar todos os grupos musculares com eficiência.'
+      'Equipamentos variados: Uso de equipamentos para trabalhar todos os grupos musculares com eficiência.'
     ],
-    conclusion: 'Ideal para quem busca superar limites, melhorar a performance atlética e alcançar novos patamares de condicionamento físico.'
+    conclusion: 'Ideal para quem busca se superar, melhorar a performance e alcançar novos patamares de condicionamento físico.'
   }
 ]
